0dfc25ae80cdb05b0198cf340b5a357dd4831505
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-01-20 20:08:14 +0100

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import fs from "fs-extra";
 
-import type { Plugin } from "rollup";
+import type { Plugin, NormalizedOutputOptions, OutputBundle } from "rollup";
 
 export default function clean(directory: string): Plugin {
   let removePromise: Promise<void> | null = null;
   return {
-    generateBundle(this, _options, _bundle, isWrite) {
+    async generateBundle(
+      _options: NormalizedOutputOptions,
+      _bundle: OutputBundle,
+      isWrite
+    ) {
       if (isWrite) {
         // Only remove before first write, but make all writes wait on the removal
         removePromise ||= fs.remove(directory);
-        return removePromise;
+        // return removePromise;
       }
     },
     name: "clean",
