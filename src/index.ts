994a673dabadd0f5cec7d55407a1a260fcb52342
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-01-20 19:32:27 +0100

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export default () => 'TS Starter Plugin';
+import fs from "fs-extra";
+
+import type { Plugin } from "rollup";
+
+export default function cleanBeforeWrite(directory: string): Plugin {
+  let removePromise: Promise<void> | null = null;
+  return {
+    generateBundle(this, _options, _bundle, isWrite) {
+      if (isWrite) {
+        // Only remove before first write, but make all writes wait on the removal
+        removePromise ||= fs.remove(directory);
+        return removePromise;
+      }
+    },
+    name: "clean-before-write",
+  };
+}
